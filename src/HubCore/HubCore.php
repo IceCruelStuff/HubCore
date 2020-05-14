f0f0eba0972644f798019c908b240364f4646d8d
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-05-13 19:22:25 -0700

    Update HubCore.php

--- src/HubCore/HubCore.php
+++ src/HubCore/HubCore.php
@@ -2,67 +2,73 @@
 
 namespace HubCore
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat;
-use pocketmine\Player;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandExecutor;
 use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\Listener;
 use pocketmine\math\Vector3;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use pocketmine\Server;
 
-class HubCore extends PluginBase implements Listener{
-	
+class HubCore extends PluginBase implements Listener {
+
 	public $prefix = "§7[§6System§7]";
 	public $error = "§7[§4ERROR§7]";
 	public $warn = "§7[§cWarn§7]";
 	public $warning = "§7[§eWarning§7]";
 	public $report = "§7[§cREPORT§7]";
-	
-	public function onLoad(){
+
+	public function onLoad() : void {
 		$this->getLogger()->info("§7Loading HubCore...");
 	}
-	
-	public function onEnable(){
-		$this->getLogger()->info("HubCore has been loaded");
-		$this->getLogger()->info("HubCore enabled");
+
+	public function onEnable() : void {
+		$this->getLogger()->info("HubCore has been enabled");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
-	
-	public function onDisable(){
+
+	public function onDisable() : void {
 		$this->getLogger()->info("HubCore has been disabled");
 	}
-	
-	public function onJoin(PlayerJoinEvent $join){
+
+	public function onJoin(PlayerJoinEvent $join) {
 		$player = $join->getPlayer();
-		$player->teleport($this->getServer()->getDefaultLevel()->getSpawnLocation());
+		$spawnLocation = $this->getServer()->getDefaultLevel()->getSpawnLocation();
+		$player->teleport($spawnLocation);
 	}
-	
-	public function onPlayerDeath(PlayerDeathEvent $e){
-		$e->setDeathMessage("You died");
+
+	public function onPlayerDeath(PlayerDeathEvent $event) {
+		$event->setDeathMessage("You died");
+		$player = $event->getPlayer();
+		$spawnLocation = $this->getServer()->getDefaultLevel()->getSpawnLocation();
+		$player->teleport($spawnLocation);
 	}
-	
-	public function onDamage(EntityDamageEvent $event){
+
+	public function onDamage(EntityDamageEvent $event) {
 		$player = $event->getEntity();
 		$default = $this->getServer()->getDefaultLevel();
-		if($player->getLevel() === $default){
+		if ($player->getLevel() === $default) {
 			$event->getCause() === EntityDamageEvent::CAUSE_FALL;
 			$event->setCancelled();
-		} else {
-			
 		}
 	}
-	
-	public function onHurt(EntityDamageEvent $eve){
+
+	public function onHurt(EntityDamageEvent $eve) {
 		$entity = $eve->getEntity();
-		$v = new Vector3($entity->getLevel()->getSpawnLocation()->getX(),$entity->getPosition()->getY(),$entity->getLevel()->getSpawnLocation()->getZ);
+		$v = new Vector3(
+			$entity->getLevel()->getSpawnLocation()->getX(), 
+			$entity->getPosition()->getY(), 
+			$entity->getLevel()->getSpawnLocation()->getZ,
+		);
 		$r = $this->getServer()->getSpawnRadius();
 		if(($entity instanceof Player) && ($entity->getPosition()->distance($v) <= $r)){
 			$eve->setCancelled();
 		}
 	}
+
 }
